22b1322921a19fb48aebc08fd53420bb3a9e7a45
lisafaggiolo <lisa.faggiolo@live.ca>
2020-09-12 20:35:56 +0000

    JS File is responsible for the functionality .counter line 63 in index.html it is responsible for counting the character the users type

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,26 @@
+
+
+
+$(document).ready(() => {
+  
+  
+  $("#tweet-text").on("keydown", () => {
+    //console.log($('#tweet-text').val().length)
+    let charLeft = 140 - $('#tweet-text').val().length;
+
+    $(".counter").text(charLeft);
+    if (charLeft < 0) {
+      $( ".counter" ).addClass( "errCounter" );
+
+    } else {
+      $('.counter').removeClass('errCounter');
+    }
+  
+  })
+
+  $("#button").on("click", () => {
+
+    $('.counter').text(140).removeClass('errCounter');
+  })
+
+})
